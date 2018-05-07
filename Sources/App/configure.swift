3962842ad9fe12714041a663ed72bbe4ec382257
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-05-07 17:25:48 -0500

    Support for 5c0421f77bec26a33a7d10696c5f5e1423b1f199. Removed Fluent dependency and sample code that depended on it. We're down to just a barebones "Hello, world" Vapor 3 example now.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,3 @@
-import FluentSQLite
 import Vapor
 
 /// Called before your application initializes.
@@ -9,9 +8,6 @@ public func configure(
     _ env: inout Environment,
     _ services: inout Services
 ) throws {
-    /// Register providers first
-    try services.register(FluentSQLiteProvider())
-
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -23,25 +19,4 @@ public func configure(
     middlewares.use(DateMiddleware.self) // Adds `Date` header to responses
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
-    // Configure a SQLite database
-    let sqlite: SQLiteDatabase
-    if env.isRelease {
-        /// Create file-based SQLite db using $SQLITE_PATH from process env
-        sqlite = try SQLiteDatabase(storage: .file(path: Environment.get("SQLITE_PATH")!))
-    } else {
-        /// Create an in-memory SQLite database
-        sqlite = try SQLiteDatabase(storage: .memory)
-    }
-
-    /// Register the configured SQLite database to the database config.
-    var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
-    services.register(databases)
-
-    /// Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
-    services.register(migrations)
-
 }
